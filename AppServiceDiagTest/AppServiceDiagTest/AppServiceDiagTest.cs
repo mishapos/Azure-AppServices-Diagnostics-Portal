8d5e748de0d9db6377c8110f7366b07042c8f7bc
XiaoxuHU <xiaoxhu@microsoft.com>
2022-06-07 11:20:32 -0700

    Add Test case

---
+++ AppServiceDiagTest/AppServiceDiagTest/AppServiceDiagTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace AppServiceDiagTest
+{
+    [TestClass]
+    public class AppServiceDiagTest
+    {
+        private IWebDriver driver;
+        [TestMethod]
+        public void TestMethod()
+        {
+            string url = "https://ms.portal.azure.com/#@microsoft.onmicrosoft.com/resource/subscriptions/1402be24-4f35-4ab7-a212-2cd496ebdf14/resourceGroups/Default-Web-WestUS/providers/Microsoft.Web/sites/buggyapp/supportrequest";
+            driver.Navigate().GoToUrl(url);
+            //Thread.Sleep(500);
+            LogIn();
+        }
+
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        }
+
+        private void LogIn()
+        {
+            var email = "xiaoxhu@microsoft.com";
+            driver.FindElement(By.Id("i0116")).SendKeys(email);
+            driver.FindElement(By.Id("i0116")).SendKeys(Keys.Enter);
+            //driver.FindElement(By.CssSelector())
+        }
+    }
+}
---
+++ AppServiceDiagTest/AppServiceDiagTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppServiceDiagTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppServiceDiagTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("369e04bd-3d56-4a09-a007-2b0fdcd4974b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
